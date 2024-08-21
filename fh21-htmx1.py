870ea53f859850f3c36444dee0b523488461aac5
gnoparus <surapong@gmail.com>
2024-08-21 15:44:38 +0700

    counter with htmx

---
+++ fh21-htmx1.py
@@ -0,0 +1,29 @@
+from fasthtml.common import *
+from starlette.testclient import TestClient
+
+
+app = FastHTML()
+
+count = 0
+
+
+@app.get("/")
+def home():
+    return Title("Count Demo"), Main(
+        H1("Count Demo"),
+        P(f"Count is set to {count}", id="count"),
+        Button(
+            "Increment", hx_post="/increment", hx_target="#count", hx_swap="innerHTML"
+        ),
+    )
+
+
+@app.post("/increment")
+def increment():
+    print("incrementing")
+    global count
+    count += 1
+    return f"Count is set to {count}"
+
+
+serve()
