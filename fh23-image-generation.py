343e83cd14eb66373738540c372d1eccbeb9715f
gnoparus <surapong@gmail.com>
2024-08-31 15:28:44 +0700

    adds more stylish ui

--- fh23-image-generation.py
+++ fh23-image-generation.py
@@ -1,23 +1,37 @@
 from fastcore.parallel import threaded
 from fasthtml.common import *
 from starlette.testclient import TestClient
-import os, uvicorn, requests, replicate
+import os, uvicorn, requests, replicate, uuid
 from dotenv import load_dotenv
 from PIL import Image
 
 # Load environment variables from the .env file (if present)
 load_dotenv()
 
-app = FastHTML(hdrs=(picolink,))
-
 # Replicate setup (for image generation)
 replicate_api_token = os.environ["REPLICATE_API_TOKEN"]
 client = replicate.Client(api_token=replicate_api_token)
 
+tables = database("data/gens.db").t
+gens = tables.gens
+
+if not gens in tables:
+    gens.create(prompt=str, id=int, folder=str, pk="id")
+Generation = gens.dataclass()
+
+# Flexbox CSS (http://flexboxgrid.com/)
+gridlink = Link(
+    rel="stylesheet",
+    href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css",
+    type="text/css",
+)
+
+
 # Store our generations
-generations = []
 folder = f"gens/"
 os.makedirs(folder, exist_ok=True)
+
+app = FastHTML(hdrs=(picolink, gridlink))
 
 
 @app.get("/")
@@ -29,29 +43,45 @@ def home():
         target_id="gen-list",
         hx_swap="afterbegin",
     )
-    gen_list = Div(id="gen-list")
+    gen_containers = [generation_preview(gen) for gen in gens(limit=10)]
+    gen_list = Div(*reversed(gen_containers), id="gen-list", cls="row")
 
     return Title("Image Generation Demo"), Main(
         H1("Image Generation Demo"), add, gen_list, cls="container"
     )
 
 
-def generation_preview(id):
-    if os.path.exists(f"gens/{id}.png"):
-        return Div(Img(src=f"/gens/{id}.png"), id=f"gen-{id}")
+def generation_preview(g: Generation):
+    grid_cls = "box col-xs-12 col-sm-6 col-md-4 col-lg-3"
+    image_path = f"{g.folder}/{g.id}.png"
+    if os.path.exists(image_path):
+        return Div(
+            Card(
+                Img(
+                    src=image_path,
+                    alt=f"Generated image for {g.prompt}",
+                    cls="card-img-top",
+                ),
+                Div(P(B("Prompt: "), g.prompt, cls="card-text")),
+                cls="card-body",
+            ),
+            id=f"gen-{g.id}",
+            cls=grid_cls,
+        )
     else:
         return Div(
-            "Generating...",
-            id=f"gen-{id}",
-            hx_post=f"/generations/{id}",
-            hx_trigger="every 1s",
+            f"Generating gen {g.id} with prompt {g.prompt}...",
+            id=f"gen-{g.id}",
+            hx_get=f"/gens/{g.id}",
+            hx_trigger="every 2s",
             hx_swap="outerHTML",
+            cls=grid_cls,
         )
 
 
-@app.post("/generations/{id}")
-def get(id: int):
-    return generation_preview(id)
+@app.get("/gens/{id}")
+def preview(id: int):
+    return generation_preview(gens.get(id))
 
 
 # For images, CSS, etc.
@@ -62,18 +92,19 @@ def static(fname: str, ext: str):
 
 @app.post("/")
 def post(prompt: str):
-    id = len(generations)
-    generate_and_save(prompt, id)
-    generations.append(prompt)
+    folder = f"data/gens/{str(uuid.uuid4())}"
+    os.makedirs(folder, exist_ok=True)
+    g = gens.insert(Generation(prompt=prompt, folder=folder))
+    generate_and_save(g.prompt, g.id, g.folder)
     clear_input = Input(
         id="new-prompt", name="prompt", placeholder="Enter a prompt", hx_swap_oob="true"
     )
-    return generation_preview(id), clear_input
+    return generation_preview(g), clear_input
 
 
 # Generate an image and save it to the folder (in a separate thread)
 @threaded
-def generate_and_save(prompt, id):
+def generate_and_save(prompt, id, folder):
     output = client.run(
         "playgroundai/playground-v2.5-1024px-aesthetic:a45f82a1382bed5c7aeb861dac7c7d191b0fdf74d8d57c4a0e6ed7d4d0bf7d24",
         input={
